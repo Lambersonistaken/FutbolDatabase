e7c7d9a7b52a51308dde0d0aaacedcd609e61f00
İsmail Emir Lambacıoğlu <73862428+Lambersonistaken@users.noreply.github.com>
2021-12-20 17:44:09 +0300

    Add files via upload

---
+++ FutbolDatabase/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace FutbolDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.baskanIdText = new System.Windows.Forms.TextBox();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.baskanAdiText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.baskanSoyadiText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.baskanListele = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(673, 433);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(741, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Başkan ID :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // baskanIdText
+            // 
+            this.baskanIdText.Location = new System.Drawing.Point(839, 29);
+            this.baskanIdText.Name = "baskanIdText";
+            this.baskanIdText.Size = new System.Drawing.Size(174, 22);
+            this.baskanIdText.TabIndex = 2;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(871, 151);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(142, 38);
+            this.btnListele.TabIndex = 3;
+            this.btnListele.Text = "TakımListele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // baskanAdiText
+            // 
+            this.baskanAdiText.Location = new System.Drawing.Point(839, 71);
+            this.baskanAdiText.Name = "baskanAdiText";
+            this.baskanAdiText.Size = new System.Drawing.Size(174, 22);
+            this.baskanAdiText.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(734, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Takım Adı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(741, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Başkan Adı : ";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(871, 195);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(142, 38);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(871, 239);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(142, 38);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(871, 283);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(142, 38);
+            this.btnGüncelle.TabIndex = 3;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // baskanSoyadiText
+            // 
+            this.baskanSoyadiText.Location = new System.Drawing.Point(839, 99);
+            this.baskanSoyadiText.Name = "baskanSoyadiText";
+            this.baskanSoyadiText.Size = new System.Drawing.Size(174, 22);
+            this.baskanSoyadiText.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(724, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Başkan Soyadı : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(734, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Takım Adı :";
+            // 
+            // baskanListele
+            // 
+            this.baskanListele.Location = new System.Drawing.Point(702, 151);
+            this.baskanListele.Name = "baskanListele";
+            this.baskanListele.Size = new System.Drawing.Size(150, 39);
+            this.baskanListele.TabIndex = 9;
+            this.baskanListele.Text = "BaşkanListele";
+            this.baskanListele.UseVisualStyleBackColor = true;
+            this.baskanListele.Click += new System.EventHandler(this.baskanListele_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 477);
+            this.Controls.Add(this.baskanListele);
+            this.Controls.Add(this.baskanSoyadiText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.baskanAdiText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.baskanIdText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox baskanIdText;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.TextBox baskanAdiText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.TextBox baskanSoyadiText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button baskanListele;
+    }
+}
+
---
+++ FutbolDatabase/Form1.cs
@@ -0,0 +1,84 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FutbolDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        NpgsqlConnection baglanti = new NpgsqlConnection("server=localHost; port=5432; Database=FutbolDB; user ID=postgres; password=2001");
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+
+            string sorgu = "SELECT * FROM \"Takım\"";
+            NpgsqlDataAdapter DA = new NpgsqlDataAdapter(sorgu,baglanti);
+            DataSet ds = new DataSet();
+            DA.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("INSERT INTO \"TakimBaskani\" (\"TakimBaskaniID\",\"BaskanAdi\",\"BaskanSoyadi\") values(@p1,@p2,@p3)",baglanti);
+            komut1.Parameters.AddWithValue("@p1", int.Parse(baskanIdText.Text));
+            komut1.Parameters.AddWithValue("@p2", baskanAdiText.Text);
+            komut1.Parameters.AddWithValue("@p3", baskanSoyadiText.Text);
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Ekleme işlemi başarı ile gerçekleşti");    
+
+
+        }
+
+        private void baskanListele_Click(object sender, EventArgs e)
+        {
+            string sorgu = "SELECT * FROM \"TakimBaskani\"";
+            NpgsqlDataAdapter DA = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            DA.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("DELETE FROM \"TakimBaskani\" WHERE \"TakimBaskaniID\"=@p1",baglanti);
+            komut2.Parameters.AddWithValue("@p1", int.Parse(baskanIdText.Text));
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Silme İşlemi Gerçekleşti");
+        }
+
+        private void btnGüncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("UPDATE \"TakimBaskani\" SET \"BaskanAdi\"=@p1, \"BaskanSoyadi\" =@p2 WHERE \"TakimBaskaniID\"=@p3",baglanti);
+            komut3.Parameters.AddWithValue("@p1",baskanAdiText.Text);
+            komut3.Parameters.AddWithValue("@p2",baskanSoyadiText.Text);
+            komut3.Parameters.AddWithValue("@p3", int.Parse(baskanIdText.Text));
+            komut3.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Güncelleme Başarılı");
+
+        }
+    }
+}
